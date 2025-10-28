faabb39adc427c698c8213d9fde82784c207f8b7
mengrui song <summerrock1017@gmail.com>
2025-10-28 21:11:40 +0900

    fix: lint

--- app/jobs/price_update_job.rb
+++ app/jobs/price_update_job.rb
@@ -5,16 +5,16 @@ class PriceUpdateJob
 
   def perform
     Rails.logger.info "Starting periodic price update job"
-    
+
     start_time = Time.current
     updated_count = 0
-    
+
     Product.all.each do |product|
       begin
         old_price = product.dynamic_price
         DynamicPricingService.new(product).calculate_dynamic_price
         new_price = product.reload.dynamic_price
-        
+
         if old_price != new_price
           updated_count += 1
           Rails.logger.info "Updated price for #{product.name}: #{old_price} -> #{new_price}"
@@ -24,7 +24,7 @@ class PriceUpdateJob
         Sidekiq.logger.error e.backtrace.join("\n")
       end
     end
-    
+
     duration = Time.current - start_time
     Rails.logger.info "Price update job completed. Updated #{updated_count} products in #{duration.round(2)} seconds"
   end
